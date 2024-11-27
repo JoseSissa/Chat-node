37506bf345a13154ec064698126444863e0978aa
Jose Sissa <sissamendoza@outlook.com>
2024-11-27 11:29:06 -0500

    Server and first view

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import logger from 'morgan'
+
+const port = process.env.PORT || 3000
+
+const app = express()
+// Logger - Nos da trazabilidad sobre las peticiones que hacemos
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
