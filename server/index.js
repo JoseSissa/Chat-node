c2502c16a2e5f948d61f8f735a6bc0ff27338762
Jose Sissa <sissamendoza@outlook.com>
2024-11-28 10:44:33 -0500

    Socket work fine in local

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT || 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+// Socket.io
+io.on('connection', (socket) => {
+  console.log('A user connected')
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message)
+  })
+})
 // Logger - Nos da trazabilidad sobre las peticiones que hacemos
 app.use(logger('dev'))
 
@@ -11,6 +29,6 @@ app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
